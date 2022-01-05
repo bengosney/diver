651b7d0bbb2976b392b1f387db460d4f32066240
Ben Gosney <bengosney@googlemail.com>
2022-01-05 13:51:06 +0000

    Liniting

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,10 +1,13 @@
 extends KinematicBody2D
 
+export(int) var speed = 100
+export(int) var jump_speed = -200
+export(int) var gravity = 800
+export(int) var buoyancy_step = 25
 
-export (int) var speed = 100
-export (int) var jump_speed = -200
-export (int) var gravity = 800
-export (int) var buoyancy_step = 25
+export(float, 0, 1.0) var friction = 0.01
+export(float, 0, 1.0) var acceleration = 0.025
+export(float, 0, 1.0) var buoyancy_acc = 0.25
 
 var velocity = Vector2.ZERO
 var minmax = 0.05
@@ -12,15 +15,12 @@ var max_buoy = gravity * (1 + minmax)
 var min_buoy = gravity * (1 - (minmax * 2))
 var buoyancy = min_buoy
 
-export (float, 0, 1.0) var friction = 0.01
-export (float, 0, 1.0) var acceleration = 0.025
-export (float, 0, 1.0) var buoyancy_acc = 0.25
 
 func get_input():
 	var mod = .5
 	if is_on_ceiling() or is_on_wall() or is_on_floor():
 		mod = 1
-		
+
 	var dir = 0
 	if Input.is_action_pressed("walk_right"):
 		dir += mod
@@ -30,19 +30,17 @@ func get_input():
 		velocity.x = lerp(velocity.x, dir * speed, acceleration)
 	else:
 		velocity.x = lerp(velocity.x, 0, friction)
-		
-	var buoy = 0;
+
+	var buoy = 0
 	if Input.is_action_pressed("buoyancy_inc"):
-		buoy = 1;
+		buoy = 1
 	if Input.is_action_pressed("buoyancy_dec"):
-		buoy = -1;
-		
+		buoy = -1
+
 	if buoy != 0:
 		var target = buoyancy + (buoy * buoyancy_step)
-		
 		buoyancy = lerp(buoyancy, max(min(target, max_buoy), min_buoy), buoyancy_acc)
-	print(mod)
-	
+
 
 func _physics_process(delta):
 	get_input()
