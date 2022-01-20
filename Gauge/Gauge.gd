3e296be5cc99254c14752f6b1df46cfd7c160b4c
Ben Gosney <bengosney@googlemail.com>
2022-01-20 09:12:50 +0000

    Gauges have min and max

--- Gauge/Gauge.gd
+++ Gauge/Gauge.gd
@@ -1,19 +1,19 @@
 extends Control
 
 export(float) var value = 100
-export(bool) var limited = true
+export(float) var min_value = 0
+export(float) var max_value = 100
 
 var _min = -121
 var _max = 121
 
 
+func _get_mapped_value():
+	return (value - min_value) / (max_value - min_value) * (_max - _min) + _min
+
+
 func move():
-	var inc = (_max - _min) / 100.0
-	var rot = _min + (inc * value)
-	if limited:
-		$Needle.rotation_degrees = max(_min, min(_max, rot))
-	else:
-		$Needle.rotation_degrees = rot
+	$Needle.rotation_degrees = max(_min, min(_max, _get_mapped_value()))
 
 
 func _ready():
@@ -22,3 +22,15 @@ func _ready():
 
 func _process(_delta):
 	move()
+
+
+func set_min(new_min):
+	self.min_value = new_min
+
+
+func set_max(new_max):
+	self.max_value = new_max
+
+
+func set_value(new_value):
+	self.value = new_value
