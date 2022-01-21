d65e49b5d229424d263e97ba9bac4d92a8738702
Ben Gosney <bengosney@googlemail.com>
2022-01-21 21:55:22 +0000

    Crabs can now turn around at the edge

--- Crab/Crab.gd
+++ Crab/Crab.gd
@@ -8,6 +8,8 @@ export(int) var speed = 50
 export(float, 0, 1.0) var acceleration = 0.25
 export(float, 0, 1.0) var friction = 0.01
 
+export(bool) var walk_off = false
+
 var velocity = Vector2.ZERO
 var direction = "LEFT"
 var walking = true
@@ -25,11 +27,35 @@ func turn_around():
 		direction = "LEFT"
 
 
+func is_on_edge():
+	var shape = $Body.get_shape()
+	var width = shape.height + (shape.radius * 2)
+
+	var offset = Vector2(width / 2, 0)
+
+	var space_state = get_world_2d().direct_space_state
+	var test
+	if direction == "RIGHT":
+		test = $Body.global_position + offset
+	else:
+		test = $Body.global_position - offset
+	var down = test + (Vector2.DOWN * shape.height * 2)
+	var result = space_state.intersect_ray(test, down, [self])
+
+	if not result:
+		return true
+
+	return false
+
+
 func walk():
 	if not is_on_floor() or not walking:
 		return
 
 	if is_on_wall():
+		turn_around()
+
+	if not walk_off and is_on_edge():
 		turn_around()
 
 	var dir = 0
@@ -45,9 +71,9 @@ func walk():
 
 
 func _physics_process(delta):
-	walk()
 	velocity.y += (gravity - buoyancy) * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
+	walk()
 
 	for i in get_slide_count():
 		var collision = get_slide_collision(i)
