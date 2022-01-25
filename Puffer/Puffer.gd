66aaa904ac56b9d7c7653949f7d652ece9f20d44
Ben Gosney <bengosney@googlemail.com>
2022-01-25 14:13:58 +0000

    Most of a puffer fish

--- Puffer/Puffer.gd
+++ Puffer/Puffer.gd
@@ -5,23 +5,52 @@ signal hit_player(node, dammage)
 enum Directions { LEFT, RIGHT }
 
 export(Directions) var starting_direction = Directions.LEFT
+export(int) var view_distance = 50
+export(float) var view_angle = 100
 
 var gravity = 800
 var buoyancy = 800
 var speed = 50
 var acceleration = 0.25
-var friction = 0.01
+var friction = 0.05
 
 var velocity = Vector2.ZERO
 var direction = Directions.LEFT
 
+var players = []
+var puffed = false
+
 
 func _ready():
 	direction = starting_direction
-	_set_sprit_flip()
+	_set_sprite_flip()
+	players = get_tree().get_nodes_in_group("player")
+	check_should_puff()
 
 
-func _set_sprit_flip():
+func draw_view_cone():
+	var thing = view_angle / 2
+
+
+func check_should_puff():
+	for player in players:
+		var dist = position.distance_to(player.get_global_position())
+		if dist > view_distance:
+			continue
+
+		var deg = rad2deg(get_angle_to(player.get_global_position()))
+
+		var to = view_angle / 2
+		var from = -to
+
+		if deg > min(to, from) and deg < max(to, from):
+			puffed = true
+			return
+
+	puffed = false
+
+
+func _set_sprite_flip():
 	$AnimatedSprite.set_flip_h(direction == Directions.LEFT)
 
 
@@ -31,7 +60,7 @@ func turn_around():
 	else:
 		direction = Directions.LEFT
 
-	_set_sprit_flip()
+	_set_sprite_flip()
 
 
 func swim():
@@ -44,10 +73,18 @@ func swim():
 	if direction == Directions.RIGHT:
 		dir += 1
 
-	if dir != 0:
+	if dir != 0 and !puffed:
 		velocity.x = lerp(velocity.x, dir * speed, acceleration)
 	else:
 		velocity.x = lerp(velocity.x, 0, friction)
+
+
+func _process(_delta):
+	check_should_puff()
+	if puffed:
+		$AnimatedSprite.frame = 1
+	else:
+		$AnimatedSprite.frame = 0
 
 
 func _physics_process(delta):
