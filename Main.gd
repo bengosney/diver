ed6e97bf4210384256fc2557bf0be5cec4a0a6cf
Ben Gosney <bengosney@googlemail.com>
2022-02-03 09:07:28 +0000

    Enter and space now start the game

--- Main.gd
+++ Main.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
 
-func _on_TitleScreen_start_game():
+func start_game():
 	get_tree().change_scene("res://MainLevel/PlayableLevel.tscn")
+
+
+func _on_TitleScreen_start_game():
+	start_game()
+
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		start_game()
