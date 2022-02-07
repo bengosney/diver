78a46a7ddbc46a3b91516cb96400b9fc7b7a147e
Ben Gosney <bengosney@googlemail.com>
2022-02-07 14:08:10 +0000

    Start of random level gen

--- TitleScreen/TitleScreen.gd
+++ TitleScreen/TitleScreen.gd
@@ -5,3 +5,19 @@ signal start_game
 
 func _on_Button_pressed():
 	emit_signal("start_game")
+
+
+func _ready():
+	var noise = OpenSimplexNoise.new()
+
+	noise.seed = 1
+	noise.octaves = 4
+	noise.period = 20
+	noise.persistence = 0.8
+
+	$Node2D/TileMap.clear()
+	for x in range(0, 40):
+		for y in range(0, 20):
+			var p = Vector2(x, y)
+			var n = noise.get_noise_2dv(p)
+			$Node2D/TileMap.set_cellv(p, n > 0, false, false, true)
